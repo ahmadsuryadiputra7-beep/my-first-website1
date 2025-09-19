8a403ea07c804a2caa995ca5d4ff6f3b439ebefd
ahmadsuryadiputra7-beep <ahmadsuryadiputra7@gmail.com>
2025-09-20 02:43:27 +0700

    Add files via upload

---
+++ website/main.js
@@ -0,0 +1,35 @@
+let variabel1 = prompt("Masukan sandi")
+if (variabel1 === ("enay")) {
+    alert("WELCOME NAYLA")
+    let variabel2 = prompt("12 + 5 =")
+    if (variabel2 === ("17")) {
+        alert("Good job")
+        let variabel3 = prompt("89 - 27 =")
+        if (variabel3 === ("62")) {
+            alert("Nice one")
+            let variabel4 = prompt("12 x 12 =")
+            if (variabel4 === ("144")) {
+                alert("Good girl that last :p")
+                let variabel5 = prompt("What is your full name")
+                let x1 = prompt("where are you born")
+                let x2 = prompt("what is your hobbies")
+                let x3 = prompt("what is your favorite color")
+                alert("my name is " + variabel5 + " born in " + x1 + " i like " + x2 + " and " + x3 + " is my favorite color")
+                alert("NAYLAAA NUGASS KAMU TEHHH!!!")
+                alert("JANGAN MALASS YAAAAA :P ")
+            } else {
+                alert("ahh you miss")
+                alert("You can refresh for try again")
+            }
+        } else {
+            alert("Nice try")
+            alert("You can refresh for try again")
+        }
+    } else {
+        alert("U ARE STUPID!")
+        alert("You can refresh for try again")
+    }
+} else {
+    alert("WRONG PASSWORD!")
+    alert("You can refresh for try again")
+}
